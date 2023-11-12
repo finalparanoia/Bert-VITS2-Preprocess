3662daf83171cf89c099489601d2cca25e0471ee
finalreality <finalparanoia@outlook.com>
2023-11-12 20:25:13 +0800

    多项修正 取消debug注释

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# from utils.create import create
-# from utils.tag import tag
-# from utils.resample import resample
-# from utils.clean import clean
+from utils.create import create
+from utils.tag import tag
+from utils.resample import resample
+from utils.clean import clean
 from utils.model_conf import gen_config
 
 
 if __name__ == "__main__":
     pass
     dataset_name = input("请为数据集命名：")
-    # create(dataset_name)
-    # resample(dataset_name)
-    # tag(dataset_name)
-    # clean(dataset_name)
+    create(dataset_name)
+    resample(dataset_name)
+    tag(dataset_name)
+    clean(dataset_name)
     gen_config(dataset_name)
